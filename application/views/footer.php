6e6c0e3e0c77fb0729ef558b3f1069a5da188d7d
Johan Rahar <johanrahar@gmail.com>
2017-08-31 11:32:55 +0700

    update 31 august

---
+++ application/views/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body class="nav-md">
+<!-- footer content -->
+<div class="container body">
+<div class="main_container">
+<footer>
+  <div class="pull-right">
+    Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+  </div>
+  <div class="clearfix"></div>
+</footer>
+</div >
+  </div >
+<!-- /footer content -->
+</body>
+</html>
