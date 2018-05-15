fd643476535c907d8b757dc6934bedc4dfd4f19b
DESKTOP-1RROORF\johan <johanrahar@gmail.com>
2018-05-16 01:24:47 +0700

    ganti title login

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -13,7 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Waroenkpos | Masuk</title>
+    <title>POS Retail | Masuk</title>
 
     <!-- Bootstrap -->
     <link href="<?php echo base_url('vendors/bootstrap/dist/css/bootstrap.min.css'); ?>" rel="stylesheet">
